e667f1cd4332549aff987c0f79d0e4cf9b200bea
Innocentia <innocentiahan@gmail.com>
2025-03-08 14:40:00 +0800

    nama class masih error tidak tau kenapa, tapi tambahkan stack untuk undo dan update deleteTask

--- src/Coba2.java
+++ src/Coba2.java
@@ -9,12 +9,18 @@ interface TaskStorage {
 }
 
 class ArrayTask implements TaskStorage {
-    private String tasksArray;
+    private String[] tasksArray;
     private int size;
+    private Stack<String[]> hist;
 
     public ArrayTask(int cap) {
         tasksArray = new String[cap];
         this.size = 0;
+        this.hist = new Stack<>();
+    }
+
+    private void saveState() {
+        hist.push(tasksArray.clone());
     }
 
     @Override
@@ -28,9 +34,28 @@ class ArrayTask implements TaskStorage {
         }
     }
 
+    @Override
+    public void deleteTask (int index) {
+        if (index >= 0 && index < size) {
+            System.out.println("Deleting \"" + tasksArray[index] + "\" from tasks list");
+            for (int i = index; i < size - 1; i++) {
+                tasksArray[i] = tasksArray[i + 1];
+            }
+            tasksArray[--size] = null;
+            System.out.println("Task deleted");
+        } else {
+            System.out.println("Index out of range");
+        }
+    }
+
+    public void updateTask (int index, String newTask) {
+        
+    }
     
 }
 
 public class Coba2 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
